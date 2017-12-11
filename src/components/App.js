ccb47796d3a4f73452ef019c5f296cc65ff08a41
Philip Van Raalte <phil.vanraalte@gmail.com>
2017-12-11 12:16:18 -0500

    Setup react router

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,20 @@
  * @date 2017-12-11
  */
 import React, {Component} from 'react';
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
+import Home from './pages/Home';
 
 class App extends Component {
   render() {
     return(
-      <div>App</div>
+      <BrowserRouter>
+        <div id="site" className="site">
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Redirect to="/"/>
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
