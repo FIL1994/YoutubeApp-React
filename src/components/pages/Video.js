3a1479b2101efd1ebc8a4a0f23dc72af99200720
Philip Van Raalte <phil.vanraalte@gmail.com>
2017-12-12 12:40:10 -0500

    Added details to Video page

--- src/components/pages/Video.js
+++ src/components/pages/Video.js
@@ -4,7 +4,7 @@
  */
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Page, EmptyState} from '../SpectreCSS';
+import {Page, EmptyState, Divider} from '../SpectreCSS';
 
 import {getVideoInfo} from '../../actions/index';
 
@@ -25,27 +25,40 @@ class Video extends Component {
       return <EmptyState title="No video found"/>;
     }
     const item = items[0];
-    const {etag, id, player: {embedHtml}, statistics: {dislikeCount, likeCount, viewCount, favoriteCount, commentCount},
+    const {etag, id, player: {embedHtml}, statistics: {dislikeCount, likeCount, viewCount},
       snippet: {
         channelId, channelTitle, description, publishedAt, tags, title
       }} = item;
 
     console.log("ITEM", item);
+    const publishedDate = (new Date(publishedAt)).toLocaleString();
     return(
       <div>
-        <h1>{title}</h1>
+        <h3>{title}</h3>
+        <div dangerouslySetInnerHTML={{__html: embedHtml}}/>
+        <div className="col-7 centered">
+          <span className="float-left">{viewCount} views</span>
+          <span className="float-right">{likeCount} likes {dislikeCount} dislikes</span>
+          <br/>
+          {tags.map(t => <span key={t} className="chip">{t}</span>)}
+          <br/>
+        </div>
+        <Divider/>
+        <div>
+          <div><span className="h6">{channelTitle}</span> - <span className="text-gray">{publishedDate}</span></div>
+          <Divider/>
+          <p>{description}</p>
+        </div>
       </div>
     );
   }
 
   render() {
-    console.log(this.props);
-
     return(
       <Page className="centered text-center">
         {this.renderVideoDetails()}
       </Page>
-    )
+    );
   }
 }
 
